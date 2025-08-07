91f6cf7846e5f49d391f911634b884f265b50ab5
Franco Emanuel Delgado <delgadofranco992@gmail.com>
2025-08-07 17:05:58 -0300

    detalles graficos y redireccion a pagina principal

--- code.js
+++ code.js
@@ -3,36 +3,37 @@ const contacto = document.getElementById("contacto");
 const paginaprincipal = document.getElementById("paginaprincipal");
 const room = document.getElementById("room");
 const restaurante = document.getElementById("restaurante");
+const exit = document.getElementById("exit");
 
 paginaprincipal.addEventListener("click",()=>{
     location.href = "../index.html"
 })
 
 room.addEventListener("click",()=>{
-    location.href = "room/room.html"
+  location.href = "room/room.html"
 })
 room.addEventListener("mouseenter",()=>{
-    room.style.color = "rgb(240, 255, 255)";
+  room.style.color = "rgb(240, 255, 255)";
 })
 room.addEventListener("mouseout",()=>{
-    room.style.color = "rgb(0, 0, 0)";
+  room.style.color = "rgb(0, 0, 0)";
 })
 
 restaurante.addEventListener("click",()=>{
-    location.href = "restaurante/restaurante.html"
+  location.href = "restaurante/restaurante.html"
 })
 restaurante.addEventListener("mouseenter",()=>{
-    restaurante.style.color = "rgb(240, 255, 255)";
+  restaurante.style.color = "rgb(240, 255, 255)";
 })
 restaurante.addEventListener("mouseout",()=>{
-    restaurante.style.color = "rgb(0, 0, 0)";
+  restaurante.style.color = "rgb(0, 0, 0)";
 })
 
 contacto.addEventListener("mouseenter",()=>{
-    contacto.style.color = "rgb(240, 255, 255)";
+  contacto.style.color = "rgb(240, 255, 255)";
 })
 contacto.addEventListener("mouseout",()=>{
-    contacto.style.color = "rgb(0, 0, 0)";
+  contacto.style.color = "rgb(0, 0, 0)";
 })
 
 paginaprincipal.addEventListener("mouseenter",()=>{
@@ -42,41 +43,52 @@ paginaprincipal.addEventListener("mouseout",()=>{
     paginaprincipal.style.color = "rgb(0, 0, 0)";
 })
 
+exit.addEventListener("click",()=>{
+  location.href = "https://franco-delgado.github.io/delgadowebs/"
+})
+exit.addEventListener("mouseenter",()=>{
+  exit.style.color = "rgb(240, 255, 255)";
+})
+exit.addEventListener("mouseout",()=>{
+  exit.style.color = "rgb(0, 0, 0)";
+})
+
+
 function clickmenu(){
-    if(barramenu.style.display != "grid"){
-        barramenu.style.display = "grid";
-        let id = null;
-        let pos = 0;
+  if(barramenu.style.display != "grid"){
+    barramenu.style.display = "grid";
+    let id = null;
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 200) {
         clearInterval(id);
-        id = setInterval(frame, 5);
-        function frame() {
-            if (pos == 200) {
-                clearInterval(id);
-            }
-            else {
-                pos++;
-                barramenu.style.height = pos + 'px';
-            }
-        }
-    }else{
-        let id = null;
-        let pos = 200;
-        clearInterval(id);
-        id = setInterval(frame, 5);
-        function frame() {
-            if (pos == 0) {
-                clearInterval(id);
-                barramenu.style.display = "none";
-            } else {
-                pos--;
-                barramenu.style.height = pos + 'px';
-            }
-        }
+      }
+      else {
+        pos++;
+        barramenu.style.height = pos + 'px';
+      }
     }
+  }else{
+    let id = null;
+    let pos = 200;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 0) {
+        clearInterval(id);
+        barramenu.style.display = "none";
+      } else {
+        pos--;
+        barramenu.style.height = pos + 'px';
+      }
+    }
+  }
 }
 
 contacto.addEventListener("click",()=>{
-    location.href = "contacto/index.html";
+  location.href = "contacto/index.html";
 })
 
 
